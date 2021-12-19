f3c361f1a5086d9e000886036a731ee12021a955
wangyuheng <wangyuheng@outlook.com>
2021-12-19 12:33:46 +0800

    return false when err

--- feishu/Identifier.go
+++ feishu/Identifier.go
@@ -2,6 +2,7 @@ package feishu
 
 import (
 	"encoding/json"
+	"github.com/sirupsen/logrus"
 
 	"github.com/geeklubcn/doorman/core"
 )
@@ -10,10 +11,22 @@ var Identifier = &identifier{}
 
 type identifier struct{}
 
-func (f *identifier) Identify(code string) core.Identification {
-	body, _ := A.getToken(code)
+func (f *identifier) Identify(code string) (core.Identification, bool) {
+	body, err := A.getToken(code)
+	if err != nil {
+		logrus.Errorf("get token fail! code:%s.", code, err)
+		return "", false
+	}
 	var t oauthToken
-	_ = json.Unmarshal(body, &t)
-	body, _ = A.getUserInfo(t.getAuthorization())
-	return core.Identification(body)
+	err = json.Unmarshal(body, &t)
+	if err != nil {
+		logrus.Errorf("token illegal! body:%s.", body, err)
+		return "", false
+	}
+	body, err = A.getUserInfo(t.getAuthorization())
+	if err != nil {
+		logrus.Errorf("get user info fail! auth:%s.", t.getAuthorization(), err)
+		return "", false
+	}
+	return core.Identification(body), true
 }
